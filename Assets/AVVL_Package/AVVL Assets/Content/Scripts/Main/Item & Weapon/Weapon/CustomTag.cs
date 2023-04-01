37267992c46137debe4469c9ef263cbc3647eb7e
Elivelton Fernando Baia Nascimento <elivelton23021998@gmail.com>
2023-04-01 19:24:15 -0300

    movendo pastas e corrigindo erros de nova versão Parte2

--- Assets/AVVL_Package/AVVL Assets/Content/Scripts/Main/Item & Weapon/Weapon/CustomTag.cs
+++
@@ -1,5 +0,0 @@
-﻿using UnityEngine;
-
-public class CustomTag : MonoBehaviour {
-    public string Tag;
-}
--- Assets/AVVL_Package/AVVL Assets/Content/Scripts/Main/UI/Notifications/HintMessageKey.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-
-public class HintMessageKey : MonoBehaviour
-{
-    public GameObject NormalKey;
-    public GameObject MouseKey;
-    public Text MessageText;
-}
